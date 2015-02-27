04071611fb63f8c32a9da9cb939d89d15842730b
Balazs Zsoldos <balazs.zsoldos@everit.biz>
2015-02-27 17:22:15 +0100

    Ongoing development

---
+++ core/src/main/java/org/everit/osgi/jetty/server/component/internal/JettyServerComponent.java
@@ -0,0 +1,118 @@
+/*
+ * Copyright (C) 2015 Everit Kft. (http://www.everit.org)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.everit.osgi.jetty.server.component.internal;
+
+import java.util.Dictionary;
+import java.util.Hashtable;
+import java.util.Map;
+
+import org.eclipse.jetty.server.ConnectionFactory;
+import org.eclipse.jetty.server.Connector;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.ServerConnector;
+import org.eclipse.jetty.server.handler.ContextHandlerCollection;
+import org.everit.osgi.ecm.annotation.Activate;
+import org.everit.osgi.ecm.annotation.Component;
+import org.everit.osgi.ecm.annotation.ConfigurationPolicy;
+import org.everit.osgi.ecm.annotation.Deactivate;
+import org.everit.osgi.ecm.annotation.ReferenceConfigurationType;
+import org.everit.osgi.ecm.annotation.ServiceRef;
+import org.everit.osgi.ecm.component.ComponentContext;
+import org.everit.osgi.ecm.component.ConfigurationException;
+import org.everit.osgi.ecm.component.ServiceHolder;
+import org.osgi.framework.ServiceRegistration;
+
+@Component(componentId = "org.everit.osgi.jetty.server.component.JettyServer",
+    configurationPolicy = ConfigurationPolicy.FACTORY)
+public class JettyServerComponent {
+
+  @ServiceRef(setter = "setConnectionFactories", optional = false,
+      configurationType = ReferenceConfigurationType.CLAUSE)
+  private ConnectionFactory[] connectionFactories;
+
+  private Server server;
+
+  private ServiceRegistration<Server> serviceRegistration;
+
+  @ServiceRef(setter = "setServletContextHandlerFactories")
+  private ServiceHolder<ServletContextHandlerFactory>[] servletContextHandlerFactories;
+
+  @Activate
+  public void activate(final ComponentContext<JettyServerComponent> componentContext) {
+
+    server = new Server();
+    ContextHandlerCollection contextHandlerCollection = new ContextHandlerCollection();
+
+    server.setHandler(contextHandlerCollection);
+
+    Connector connector = new ServerConnector(server, connectionFactories);
+
+    server.addConnector(connector);
+
+    addServletContextsToServer(contextHandlerCollection);
+
+    Dictionary<String, Object> serviceProps = new Hashtable<String, Object>(
+        componentContext.getProperties());
+
+    try {
+      server.start();
+    } catch (Exception e) {
+      try {
+        server.stop();
+        server.destroy();
+      } catch (Exception stopE) {
+        e.addSuppressed(stopE);
+      }
+      // TODO throw e;
+      e.printStackTrace();
+      return;
+    }
+    serviceRegistration = componentContext.registerService(Server.class, server, serviceProps);
+  }
+
+  private void addServletContextsToServer(final ContextHandlerCollection contextHandlerCollection) {
+
+    for (ServiceHolder<ServletContextHandlerFactory> serviceHolder : servletContextHandlerFactories) {
+      Map<String, Object> attributes = serviceHolder.getAttributes();
+      Object contextPath = attributes.get("contextPath");
+
+      if (contextPath == null) {
+        throw new ConfigurationException("'contextPath' attribute must be provided in clause");
+      }
+
+      ServletContextHandlerFactory servletContextHandlerFactory = serviceHolder.getService();
+
+      contextHandlerCollection.addHandler(servletContextHandlerFactory
+          .createServletContextHandler(String.valueOf(contextPath)));
+    }
+  }
+
+  @Deactivate
+  public void deactivate() {
+    if (serviceRegistration != null) {
+      serviceRegistration.unregister();
+    }
+  }
+
+  public void setConnectionFactories(final ConnectionFactory[] connectionFactories) {
+    this.connectionFactories = connectionFactories;
+  }
+
+  public void setServletContextHandlerFactories(
+      final ServiceHolder<ServletContextHandlerFactory>[] servletContextHandlerFactories) {
+    this.servletContextHandlerFactories = servletContextHandlerFactories;
+  }
+}
